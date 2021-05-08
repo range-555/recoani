d7a7354f364830f57a076fdf2c131861f791d2e8
toshi <iskwts@gmail.com>
2021-05-08 23:28:21 +0900

    [update]web 単体イメージで動作するよう変更

--- recoani_web/recoani_web/recommend/urls.py
+++ recoani_web/recoani_web/recommend/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = "recommend"
 urlpatterns = [
@@ -9,3 +10,5 @@ urlpatterns = [
     path('result/', views.result, name='result'),
     path('anime-autocomplete/', views.anime_autocomplete, name='anime-autocomplete'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
